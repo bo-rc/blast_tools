4b747e1d8f5d0ba54b7aa6647a4522df2dd8787b
Bo Liu <bo-rc@acm.org>
2015-12-16 13:05:34 -0600

    adds max hit number to output filename

--- launch_blastp_formater.sh
+++ launch_blastp_formater.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # try all the evalues in the following list
-for e in 0.01 0.005 0.001
+for e in 0.01
 do
 ./blastp_formater.sh \
     --input-fasta M_myc_aaRS.txt \
     --output-filename report \
     --database Ecoli.fasta \
     --clean \
-    --num-report-hits 2 \
+    --num-report-hits 1 \
     --evalue $e
 done
